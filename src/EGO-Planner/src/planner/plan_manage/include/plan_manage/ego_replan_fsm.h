74c040b56ba737b6048e32612c6b218cfdc2c6d6
李嘉伦 <13613936870@163.com>
2026-03-30 22:16:52 +0800

    state1 ok

--- src/EGO-Planner/src/planner/plan_manage/include/plan_manage/ego_replan_fsm.h
+++ src/EGO-Planner/src/planner/plan_manage/include/plan_manage/ego_replan_fsm.h
@@ -89,6 +89,8 @@ namespace ego_planner
     Eigen::Vector3d odom_pos_, odom_vel_, odom_acc_;     // odometry state
     std::vector<Eigen::Vector3d> wps_;
     Eigen::Vector3d last_wp;
+    Eigen::Vector3d last_end_vel;
+    double max_acc_;
     /* ROS utils */
     ros::NodeHandle node_;
     ros::Timer exec_timer_, safety_timer_;
